156d73948052877cb78af15c163e8f59cb95e9bd
ron19907 <ronaldmweema@gmail.com>
2025-03-20 04:51:11 +0200

    added links to the footer icons

--- src/components/footerSection.vue
+++ src/components/footerSection.vue
@@ -6,13 +6,14 @@
         </div>
         <p class="footer-text">Powered by Ronald Mweema</p>
         <div class="social-links">
-          <a href="#" class="social-link">
+          <a href="https://x.com/RMweema" class="social-link">
             <twitter-icon class="icon" />
           </a>
-          <a href="#" class="social-link">
-            <instagram-icon class="icon" />
+          <a href="https://web.facebook.com/ronald.mweema.3" class="social-link">
+            <FacebookIcon class="icon" />
           </a>
-          <a href="#" class="social-link">
+          <a href="https://www.linkedin.com/in/ronald-mweema-664276309?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+" class="social-link">
             <linkedin-icon class="icon" />
           </a>
         </div>
@@ -21,7 +22,7 @@
   </template>
   
   <script setup>
-  import { Twitter as TwitterIcon, Instagram as InstagramIcon, Linkedin as LinkedinIcon } from 'lucide-vue-next';
+  import { Twitter as TwitterIcon, Linkedin as LinkedinIcon, FacebookIcon as FacebookIcon} from 'lucide-vue-next';
   </script>
   
   <style scoped>
